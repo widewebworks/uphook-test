7c5022c3003eae20f112c65e27349a4101775cd4
root <root@ip-10-62-97-129.ec2.internal>
2012-06-20 03:45:22 +0000

    sad

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// a
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-
-/**
- * Create app.
- */
-
-var app = express.createServer()
-
-/**
- * Export app.
- */
-
-module.exports = app;
-
-/**
- * Sample route.
- */
-
-app.get('/', function (req, res) {
-  res.send('Bye11122222!');
-});
-=======
 // a
 /**
  * Module dependencies.
@@ -52,4 +24,4 @@ module.exports = app;
 app.get('/', function (req, res) {
   res.send('OMGHI2U2!');
 });
->>>>>>> 8cedd11285d545e7da0c2d28b1f6559906eef223
+
